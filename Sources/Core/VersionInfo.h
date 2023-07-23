6ffaae4140707d2ee1cbdd686101fa5c157fb2f9
unknown <viereck.q@gmail.com>
2023-07-23 10:00:33 +0200

    spaces to tab. versioninfo.h
    why was there spaces even

--- Sources/Core/VersionInfo.h
+++ Sources/Core/VersionInfo.h
@@ -4,7 +4,7 @@
 
 class VersionInfo {
 public:
-    static std::string GetOperatingSystem();
+	static std::string GetOperatingSystem();
 	static std::string GetVersionInfo() {
 		std::string os = GetOperatingSystem();
 		std::string version4spades = "  |  IV of Spades  " GIT_COMMIT_HASH;
